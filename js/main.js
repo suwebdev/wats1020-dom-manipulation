1163aa3a8b0a85ee45e872707da9650b472b2c68
lewisj13 <lewisj13@seattleu.edu>
2017-04-21 17:25:46 -0700

    Updated js error

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ $( document ).ready(function() {
         greatest: 0,
         total: 0
     };
-    // Place all your Javascript code inside this "document ready" function so
-    // it does not run until the DOM is ready for Javascript manipulation.
 
     // TODO: Create a function to listen for clicks on the "login" button.
     //      1. When a user clicks the "login" button, hide the login
@@ -23,13 +21,12 @@ $( document ).ready(function() {
     //      (NOTE: You do not have to perform any validation on the data as
     //          a base requirement.)
 
-$('#login-form.btn') .on('click', function(){
-$('#login-form').hide();
- $('.user-fullname').text (userinfo.firstName + '' + userinfo.lastName);
- $('.user-fullname').show();
+$('#login-form .btn').on('click', function(event) {
+ $('#login-form').hide();
+ $('.user-info').show();
+ $('.user-fullname').text('' + userInfo.firstName + '' + userInfo.lastName);
+
 });
-
-
 
     // TODO: Create a function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
@@ -41,17 +38,16 @@ $('#login-form').hide();
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
 
-$('.view-details') .on('click', function(event) {
+$('.view-details').on('click', function(event){
     console.log(event);
   var targetElement = event.target;
   var container = targetElement.parentElement.parentElement;
 
-$(container).find('.details').each (function (index, el) {
-  if ($(el).is(':visible')) {
+$(container).find('.details').each(function(index, el){
+  if ($(el).is(':visible')){
     $(el).fadeOut();
-    targetElement.innerText="View Details"
-  }
-  else {
+    targetElement.innerText= "View Details"
+  } else {
     $(el).fadeln();
      targetElement.innerText= "Hide Details"
      }
@@ -68,19 +64,24 @@ $(container).find('.details').each (function (index, el) {
     //      4. Determine the respective percentages (out of 100) for each progress bar.
     //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
 
-$('.vote') .on('click', function (event) {
-  ($(this).attr('data-vote')  === 'great'); {
-   voteCounts.great = voteCounts.great + 1;
+$('.vote').on('click', function(event){
+    console.log(event);
+
+   if ($(this).attr('data-vote')==='great'){
+      voteCounts.great++;
+      voteCounts.total++;
+
+  } else {
+    if ($(this).attr('data-vote')==='greatest'{
+      voteCounts.greatest++;
+      voteCounts.total++;
+    }
   }
-  {voteCounts.greatest = votCounts.greatest + 1;}
 
-  voteCounts.total = voteCounts.total +1;
+  var greatPercent = voteCounts.great/ voteCounts.total * 100 + '%';
+  var greatestPercent = voteCounts.greatest/ voteCounts.total * 100 + '%';
 
-  var greatPrecent = voteCounts.great/voteCounts.total *100;
-  var greatestPercent = voteCounts.greatest/voteCounts.total*100;
-
-  $('great-Progress').css ('width', greatPercent + '%');
-  $('.greatest-Progress').css ('width', greatestPercent + '%');
-
-});
+  $('great-progress').css('width', greatPercent);
+  $('.greatest-progress').css('width', greatestPercent);
+  });
 });
